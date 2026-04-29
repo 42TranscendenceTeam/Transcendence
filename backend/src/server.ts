2e1fe68162a88906933a2c77fd4ce53ac3d090c4
diogo-adao <diogoadao01x@gmail.com>
2026-04-29 04:11:03 +0100

    removed middleware test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,6 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import authRoutes from './auth/auth.routes.js';
 
-import { authMiddleware } from './middleware/auth.middleware.js';
-import type { AuthRequest } from './middleware/auth.middleware.js';
-
 const app = express();
 const PORT = 3000;
 
@@ -15,13 +12,6 @@ app.use('/auth', authRoutes);
 
 app.get('/health', (req: Request, res: Response) => {
   res.json({ status: 'ok' });
-});
-
-app.get('/users/me', authMiddleware, (req: AuthRequest, res) => {
-  res.json({
-    message: 'You are authenticated',
-    user: req.user,
-  });
 });
 
 app.listen(PORT, () => {
